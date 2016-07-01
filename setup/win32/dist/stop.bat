a2fc057cc30672f53d377dbe2ac61c7db23462de
liemnc <ncliam@gmail.com>
2016-07-01 18:11:04 +0700

    Make Setup

---
+++ setup/win32/dist/stop.bat
@@ -0,0 +1,2 @@
+@PATH=%WINDIR%\system32;%WINDIR%;%WINDIR%\System32\Wbem;.
+@net stop odoo-server-8.0
---
+++ setup/win32/start.bat
@@ -0,0 +1,3 @@
+@PATH=%WINDIR%\system32;%WINDIR%;%WINDIR%\System32\Wbem;.
+@net stop odoo-server-8.0
+@net start odoo-server-8.0
